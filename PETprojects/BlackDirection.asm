9e86e195d05a23eb16027a7396dbb144458375ef
Zer0Hiro <95877340+Zer0Hiro@users.noreply.github.com>
2025-12-03 20:44:00 +0200

    PET1

---
+++ PETprojects/BlackDirection.asm
@@ -0,0 +1,141 @@
+@16384
+D = A
+@SP
+M = D + 1
+@4111
+D = A
+@SCREEN
+D = D + A
+@position
+M = D
+//KBD input
+(KEY_INPUT)
+    @KBD
+    D = M
+    @132
+    D = D - A
+    @GO_RIGHT
+    D;JEQ
+    @KBD
+    D = M
+    @BUTTON_PRESSED
+    D;JGT
+    @KEY_INPUT
+    0;JMP
+(BUTTON_PRESSED)
+    @KBD
+    D = M
+    @KEY_INPUT
+    D;JEQ
+    @BUTTON_PRESSED
+    0;JMP
+ 
+(GO_RIGHT) // ->
+//push y
+    @position
+    D = M
+    //Check first if its first line of screen
+    @16896
+    D = D - A
+    @KEEP
+    D;JLE
+    //Don't keep pos
+    @511
+    D = A
+    @position
+    M = M - D
+(KEEP)
+    @position
+    D = M
+    @SP
+    AM = M - 1
+    M = D
+    //push RET
+    @RET
+    D = A
+    @SP
+    AM = M - 1
+    M = D
+    @BLACK_SQUARE
+    0;JMP
+
+(GO_LEFT) // <-
+//push y
+    @position
+    D = M
+    //Check first if its first line of screen
+    @16896
+    D = D - A
+    @KEY_INPUT
+    D;JLE
+    //Don't keep pos
+    @511
+    D = A
+    @position
+    M = M - D
+(KEEP)
+    @position
+    D = M
+    @SP
+    AM = M - 1
+    M = D
+    //push RET
+    @RET
+    D = A
+    @SP
+    AM = M - 1
+    M = D
+    @BLACK_SQUARE
+    0;JMP
+
+
+(RET)
+    @position
+    M = D
+    @2
+    D = A
+    @SP
+    M = M + D
+    @KEY_INPUT
+    0;JMP
+
+// Print Black Square
+(BLACK_SQUARE)
+    // get y coordinate
+    @SP
+    A = M + 1
+    D = M
+    @y
+    M = D
+    // start counter (fine until 16)
+    @counter
+    M = 0
+(LOOP)
+    @16
+    D = A
+    @counter
+    D = M - D
+    @END_PROC
+    D;JEQ
+
+    @32767
+    D = A
+    @y
+    A = M
+    M = D
+    @32
+    D = A
+    @y
+    M = M + D
+    @counter
+    M = M + 1
+    @LOOP
+    0;JMP
+
+(END_PROC)
+    @y
+    D = M
+    @SP
+    A = M
+    A = M
+    0;JMP
---
+++ PETprojects/BlackScreen.asm
@@ -0,0 +1,12 @@
+@SCREEN
+D = A - 1
+@position
+M = D 
+(LOOP)
+@position
+AM = M + 1
+M = -1
+@LOOP
+0;JMP
+
+
